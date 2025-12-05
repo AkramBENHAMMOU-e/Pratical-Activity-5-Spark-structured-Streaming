e79ca0a2d7f5c628e0090fe04fcb255f4c0fde40
Akram BENHAMMOU <aben200244@gmail.com>
2025-12-05 11:08:54 +0100

    ignorer les logs

--- src/main/java/com/tp/Main.java
+++ src/main/java/com/tp/Main.java
@@ -1,3 +1,5 @@
+package com.tp;
+
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
@@ -15,16 +17,17 @@ public class Main {
         SparkSession ss=SparkSession.builder()
                 .appName("Structured streaming App")
                 .getOrCreate();
+        ss.sparkContext().setLogLevel("WARN");
         StructType schema = new StructType(new StructField[]{
-                new StructField("order_id",DataTypes.LongType,false, Metadata.empty()),
-                new StructField("client_id",DataTypes.LongType,false, Metadata.empty()),
-                new StructField("client_name",DataTypes.StringType,false, Metadata.empty()),
-                new StructField("product",DataTypes.StringType,false, Metadata.empty()),
-                new StructField("quantity",DataTypes.IntegerType,false, Metadata.empty()),
-                new StructField("price",DataTypes.DoubleType,false, Metadata.empty()),
-                new StructField("order_date",DataTypes.StringType,false, Metadata.empty()),
-                new StructField("status",DataTypes.StringType,false, Metadata.empty()),
-                new StructField("total",DataTypes.DoubleType,false, Metadata.empty())
+                new StructField("order_id", DataTypes.LongType, false, Metadata.empty()),
+                new StructField("client_id", DataTypes.StringType, false, Metadata.empty()), // CORRIGÉ: Long -> String
+                new StructField("client_name", DataTypes.StringType, false, Metadata.empty()),
+                new StructField("product", DataTypes.StringType, false, Metadata.empty()),
+                new StructField("quantity", DataTypes.IntegerType, false, Metadata.empty()),
+                new StructField("price", DataTypes.DoubleType, false, Metadata.empty()),
+                new StructField("order_date", DataTypes.StringType, false, Metadata.empty()),
+                new StructField("status", DataTypes.StringType, false, Metadata.empty()),
+                new StructField("total", DataTypes.DoubleType, false, Metadata.empty())
         });
 
         Dataset<Row> inputDF = ss.readStream().schema(schema).option("header",true).csv("hdfs://namenode:8020/data");
